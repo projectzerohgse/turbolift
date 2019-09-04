6af42271c802cda17010a9bf02993cc87df10b1b
Matt Riecken <matt@learningkeyworks.com>
2019-09-04 12:39:28 -0400

    Testing LTI integration

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,7 +1,28 @@
 class WelcomeController < ApplicationController
 
   def index
+    
+    $oauth_key = "test"
+    $oauth_secret = "secret"
+
+    begin
+      signature = OAuth::Signature.build(request, :consumer_secret => $oauth_secret)
+      signature.verify or raise OAuth::Unauthorized
+    rescue OAuth::Signature::UnknownSignatureMethod,
+        OAuth::Unauthorized
+      return %{unauthorized attempt. make sure you used the consumer secret "#{$oauth_secret}"}
+    end
   end
+
   def create
+
+    begin
+      signature = OAuth::Signature.build(request, :consumer_secret => $oauth_secret)
+      signature.verify or raise OAuth::Unauthorized
+    rescue OAuth::Signature::UnknownSignatureMethod,
+        OAuth::Unauthorized
+      return %{unauthorized attempt. make sure you used the consumer secret "#{$oauth_secret}"}
+    end
+
   end
 end
