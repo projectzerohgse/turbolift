d46e3e110f130db2325e0506325bce82903493bc
Matt Riecken <matt@learningkeyworks.com>
2019-09-09 13:35:03 -0400

    Add skip_before_action authenticate for survey success complete page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,4 @@
 class ApplicationController < ActionController::Base
   # before_action :authenticate_user!
-  skip_before_action :verify_authenticity_token, only: :launch
+  skip_before_action :verify_authenticity_token, only: [:launch, :complete]
 end
