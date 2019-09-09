28d2491fb85a708505145485576d61630f067508
Matt Riecken <matt@learningkeyworks.com>
2019-09-09 15:15:36 -0400

    Functionality to redirect if user has already completed the survey

--- app/controllers/survey_controller.rb
+++ app/controllers/survey_controller.rb
@@ -45,7 +45,6 @@ class SurveyController < ApplicationController
   end
 
   def complete
-
     canvas = Canvas::API.new(:host => "https://harvard-catalog-courses.instructure.com", :token => "4860~FxRNPxhS0CfekJsWBaidgK8ASZACEpwMWHKhsWCdCrRbAoP1kBfxIE4FdylhZ7Zi")
     url = "/api/v1/courses/#{session[:course_id]}/assignments/#{session[:assignment_id]}/submissions/#{session[:user_id]}?submission[posted_grade]=complete"
     canvas.put(url)
