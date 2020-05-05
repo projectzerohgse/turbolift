aec0f7f82832d882c425578550d105065dd82a3e
Matt Riecken <matt@learningkeyworks.com>
2020-05-05 12:50:44 -0400

    Typo in qualtrics url

--- app/controllers/survey_controller.rb
+++ app/controllers/survey_controller.rb
@@ -49,7 +49,7 @@ class SurveyController < ApplicationController
        url = "api/v1/courses/#{session[:course_id]}"
        response = canvas.get(url)
        course_name = response['name']
-       redirect_to generate_url("https://harvard.az1.qualtrics.com/jfe/form/SV_a4ezn525A0CoS3P", :canvas_user_id => "#{session[:user_id]}", :canvas_course_id => "#{session[:course_id]}", :canvas_course_name => course_name)
+       redirect_to generate_url("https://harvard.az1.qualtrics.com/jfe/form/SV_a4ezn525A0CoS3P", :canvas_user_id => "#{session[:user_id]}", :canvas_course_id => "#{session[:course_id]}", :canvas_course_name => "#{course_name}")
     end
 
   end
